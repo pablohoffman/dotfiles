861bc8b6f64682509adf37de4842506898411d3d
Pablo Hoffman <pablo@pablohoffman.com>
2012-03-15 10:04:01 -0300

    modified rcfiles

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -181,6 +181,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "Transmageddon.py" },
       properties = { floating = true } },
+    { rule = { class = "HipChat" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
